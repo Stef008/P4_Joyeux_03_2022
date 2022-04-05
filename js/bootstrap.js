39d87f081f4e180e78108d32792b0eff49befc32
stef008 <stephaniejoyeux8@gmail.com>
2022-04-05 18:12:41 +0200

    Modification des liens du header,ajout site map, arrangement pour w3c

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,7 +3,7 @@
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under the MIT license
  */
-if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requires jQuery"); + function(a) {
+if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requires jQuery"), + function(a) {
     "use strict";
     var b = a.fn.jquery.split(" ")[0].split(".");
     if (b[0] < 2 && b[1] < 9 || 1 == b[0] && 9 == b[1] && b[2] < 1) throw new Error("Bootstrap's JavaScript requires jQuery version 1.9.1 or higher")
